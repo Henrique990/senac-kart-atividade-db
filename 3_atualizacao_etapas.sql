d987106ac944427080df624590535c4364cd27fa
Henrique Teixeira <henriqueteixeiara26@gmail.com>
2025-11-16 23:48:00 -0300

    Atividade 2 UC3 concluida

--- 3_atualizacao_etapas.sql
+++ 3_atualizacao_etapas.sql
@@ -1,14 +1,20 @@
 USE kart_associacao;
 
-/* 1. Altera Campo Grande para Salvador */
+/* 1. Desativa o modo de segurança para permitir o UPDATE */
+SET SQL_SAFE_UPDATES = 0;
+
+/* 2. Altera Campo Grande para Salvador */
 UPDATE Etapa
 SET local = 'Salvador'
 WHERE local = 'Campo Grande';
 
-/* 2. Altera Londrina para Goiânia */
+/* 3. Altera Londrina para Goiânia */
 UPDATE Etapa
 SET local = 'Goiânia'
 WHERE local = 'Londrina';
+
+/* 4. Reativa o modo de segurança (boa prática) */
+SET SQL_SAFE_UPDATES = 1;
 
 /* Verificação (opcional) */
 SELECT * FROM Etapa;
